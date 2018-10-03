19a14313f0903ee3aef574e0c6f205ba3bf2c323
vkovec <viktoria.kovecses@gmail.com>
2018-10-03 15:29:27 -0400

    import package with bootstrap.cs on Unity start
    - for now will need to manually create the package containing Bootstrap.cs and place it into the path/to/github/startup/shotgunBootstrap.unitypackage

---
+++ startup/Shotgun/tk-unity/Bootstrap.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+
+[InitializeOnLoad]
+class Bootstrap
+{
+    static Bootstrap()
+    {
+        UnityEngine.Debug.Log("Invoking Shotgun Toolkit bootstrap script.");
+        PythonRunner.RunFile(System.Environment.GetEnvironmentVariable("SHOTGUN_UNITY_BOOTSTRAP_LOCATION"));
+    }
+}
