5e07f294f158a2f78e8c1c94d3ec75b922af6460
vkovec <viktoria.kovecses@gmail.com>
2018-10-11 15:35:22 -0400

    update Bootstrap.cs and upload custom recorder implementation
    - modified bootstrap to check for "BOOTSTRAP_SG_ON_UNITY_STARTUP" environment variable to see if we should bootstrap on startup or not (not for publishing, yes for loading)
    - modified the movie recorder to include a "publish to shotgun" checkbox in the UI. This will record the video to a temporary location and bootstrap to shotgun on completion, then open the publish window
    - include the custom recorder and updated bootstrap in the startup package

--- startup/Shotgun/tk-unity/Bootstrap.cs
+++ startup/Shotgun/tk-unity/Bootstrap.cs
@@ -4,12 +4,23 @@ using UnityEditor;
 using UnityEngine;
 using System.IO;
 
-class Bootstrap
+public class Bootstrap
 {
-    [UnityEditor.Callbacks.DidReloadScripts]
-    public static void OnReload()
+    public static void CallBootstrap()
     {
         UnityEngine.Debug.Log("Invoking Shotgun Toolkit bootstrap script.");
         PythonRunner.RunFile(System.Environment.GetEnvironmentVariable("SHOTGUN_UNITY_BOOTSTRAP_LOCATION"));
+    }
+
+    [UnityEditor.Callbacks.DidReloadScripts]
+    public static void OnReload()
+    {
+        // check environment variable to see if we want to bootstrap
+        var bootstrap = System.Environment.GetEnvironmentVariable("BOOTSTRAP_SG_ON_UNITY_STARTUP");
+        if (bootstrap == null)
+        {
+            return;
+        }
+        CallBootstrap();
     }
 }
