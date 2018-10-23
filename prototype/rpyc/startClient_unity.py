d2796da0ea172ef45aa59e040fc31bf48df7863a
David Lassonde <david.lassonde@imaginary-spaces.com>
2018-10-23 14:22:32 -0400

    wip

--- prototype/rpyc/startClient_unity.py
+++ prototype/rpyc/startClient_unity.py
@@ -3,53 +3,35 @@ import threading
 import sys
 import os
 import time
-import polling_server
 
-_shotgun_is_initialized = False    
-class MyClientSlaveService(polling_server.PollingSlaveService):
+
+class MyClientSlaveService(rpyc.core.service.SlaveService):
     def execute(self, text):
         print('[rpyc-client] Executing %s'%text)
-        
         try:
             super(MyClientSlaveService, self).execute(text)
         except:
             import traceback
             traceback.print_exc()
-        print('[rpyc-client] Done executing %s'%text)
-        
-    def on_server_stop(self):
-        print('[rpyc-client] Got on_server_stop. Resetting the connection')
 
-        import unity_connection
-        unity_connection.reset_unity_connection()
-
-    def bootstrap_shotgun(self,path_to_bootstrap):
-        global _shotgun_is_initialized
-        print('[rpyc-client] Got bootstrap_shotgun ("%s"). _shotgun_is_initialized = %s'%(path_to_bootstrap,_shotgun_is_initialized))
-        if _shotgun_is_initialized:
-            return
-        
-        execfile(path_to_bootstrap)
-        _shotgun_is_initialized = True
-
-        print('[rpyc-client] Shotgun has been initialized')
-
-if __name__ == "__main__":
-    rpyc_client = polling_server.PollingServer(MyClientSlaveService, port=18862)
-    polling_server.PollingServerUtil.init()
-    
-    rpyc_client_thread = threading.Thread(target = rpyc_client.start, name = "rpyc_client")
-    rpyc_client_thread.start()
-    
-    while True:
-        time.sleep(0.001)
-        polling_server.PollingServerUtil.dispatch_data()
-        
+    def process_qt_events(self, text):
         try:
             from sgtk.platform.qt import QtGui
             QtGui.QApplication.instance().processEvents() 
         except:
             pass
+
+if __name__ == "__main__":
+    print("[rpyc-client] Starting the rpyc client")
+    try:
+        rpyc_client = rpyc.utils.server.OneShotServer(MyClientSlaveService, port=18862)
+
+        print("[rpyc-client] rpyc_client = %s"%rpyc_client)
+       
+        # This will block the client in slave mode
+        rpyc_client.start()
+    except e:
+        print("[rpyc-client] Got an exception %s"%e)
         
     print("[rpyc-client] Bye")
         
