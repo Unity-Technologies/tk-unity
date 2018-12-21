a1eef89391ea87b9d04fdfe1f37f016c8fc4db24
vkovec <viktoria.kovecses@gmail.com>
2018-12-21 14:23:35 -0500

    remove unused settings function

--- hooks/tk-multi-publish2/basic/collector.py
+++ hooks/tk-multi-publish2/basic/collector.py
@@ -21,31 +21,6 @@ class UnitySessionCollector(HookBaseClass):
     collector hook.
     """
 
-    @property
-    def settings(self):
-        """
-        Dictionary defining the settings that this collector expects to receive
-        through the settings parameter in the process_current_session and
-        process_file methods.
-
-        A dictionary on the following form::
-
-            {
-                "Settings Name": {
-                    "type": "settings_type",
-                    "default": "default_value",
-                    "description": "One line description of the setting"
-            }
-
-        The type string should be one of the data types that toolkit accepts as
-        part of its environment configuration.
-        """
-
-        # grab any base class settings
-        collector_settings = super(UnitySessionCollector, self).settings or {}
-
-        return collector_settings
-
     def _collect_file(self, parent_item, path):
         """
         Process the supplied file path.
