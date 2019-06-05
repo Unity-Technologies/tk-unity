986054d1ec69f4e338cf5f0ef6eed5c12eaa2ce0
David Lassonde <39058799+lassond@users.noreply.github.com>
2019-06-05 11:50:55 -0400

    Ut 2235 remove useless engine argument from post init hook (#78)
    * Removed useless arg (engine)

--- hooks/bootstrap/post_init.py
+++ hooks/bootstrap/post_init.py
@@ -3,7 +3,7 @@ import sgtk
 HookBaseClass = sgtk.get_hook_baseclass()
 
 class UnityPostBootstrap(HookBaseClass):
-    def on_post_init(self, engine):
+    def on_post_init(self):
         """
         This method is invoked when tk-unity has successfully initialized
         """
