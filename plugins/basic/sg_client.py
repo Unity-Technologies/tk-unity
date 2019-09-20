fe0d6af7582017f5e2ef23c92ab51ea5dcb4a5a4
David Lassonde <david.lassonde@imaginary-spaces.com>
2019-09-20 12:03:55 -0400

    * bad merge, syncing with origin/master

--- plugins/basic/sg_client.py
+++ plugins/basic/sg_client.py
@@ -201,7 +201,7 @@ def connect_to_unity(wait_for_server = False):
             # Give the server time to restart
             import traceback, pprint
             log('Got Exception while connecting: {}. Waiting for 1 second before trying again'.format(e))
-            log('Stack trace:\n\n{}'.format(traceback.format_exc()))
+            log('Stack trace:\n\n{}'.format(pprint.pformat(traceback.format_stack())))
             time.sleep(1)
         else:
             log('Connected')
